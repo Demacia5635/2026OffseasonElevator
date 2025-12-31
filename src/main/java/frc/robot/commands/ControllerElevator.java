67d20a4b884ff5bb7815cc862b477f15df70d010
matan <diz.matan@gmail.com>
2025-12-31 19:58:39 +0200

    created the controller class

---
+++ src/main/java/frc/robot/commands/ControllerElevator.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.demacia.utils.Controller.CommandController;
+import frc.robot.subsystems.ElevatorSubsystem;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class ControllerElevator extends Command {
+  /** Creates a new ControllerElevator. */
+  ElevatorSubsystem elevatorSubsystem;
+  CommandController controller;
+  private double joyright;
+  public ControllerElevator(CommandController controller,  ElevatorSubsystem elevatorSubsystem) {
+    this.controller = controller;
+    this.elevatorSubsystem = elevatorSubsystem;
+    SmartDashboard.putData(elevatorSubsystem);
+    addRequirements(elevatorSubsystem);
+
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    joyright=controller.getRightY();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
